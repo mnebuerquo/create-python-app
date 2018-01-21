f35285603ac9e6d8cb245527a4419e378f7d68ea
mnebuerquo <mnebuerquo@users.noreply.github.com>
2018-01-21 08:42:16 -0500

    fixing download of files

--- mn_cpa_install.sh
+++ mn_cpa_install.sh
@@ -9,19 +9,29 @@
 # Run it any time to upgrade to the newest version of this tool.
 # Happy Pythoning!
 
-REPO="https://github.com/mnebuerquo/create-python-app/blob/master"
+RAW="https://raw.githubusercontent.com/mnebuerquo/create-python-app/master/"
 
 # I want to download files from my git repo, but without the .git and stuff.
 # This is easiest with just a wget, but it's tedious since I have to
 # download each file.
 
-wget "$REPO/mn_build"
-wget "$REPO/mn_cpa_install.sh"
-wget "$REPO/mn_Dockerfile"
-wget "$REPO/mn_image"
-wget "$REPO/mn_lint"
-wget "$REPO/mn_run"
-wget "$REPO/mn_test"
-wget "$REPO/pip"
-wget "$REPO/pytest.ini"
-wget "$REPO/python"
+get_executable() {
+	wget "$RAW/$1" -O "$1"
+	chmod +x "$1"
+}
+
+get_file() {
+	wget "$RAW/$1" -O "$1"
+}
+
+get_executable mn_build
+get_executable mn_cpa_install.sh
+get_executable mn_image
+get_executable mn_lint
+get_executable mn_run
+get_executable mn_test
+get_executable pip
+get_executable python
+
+get_file mn_Dockerfile
+get_file pytest.ini
